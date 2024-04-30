4925f0158579dca4f331cf15e4eb40535b6cc2f0
dpGuntisg <crazymangaming111@gmail.com>
2024-05-01 00:19:15 +0300

    register api

--- laravel/app/Http/Controllers/Api/UserController.php
+++ laravel/app/Http/Controllers/Api/UserController.php
@@ -4,8 +4,49 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    //
+    // Reguster API (POST)
+    public function register(Request $request){
+
+        // Data validation
+        $request->validate([
+            "name" => "required",
+            "email" => "required|email|unique:users",
+            "password" => "required|confirmed"
+        ]);
+
+        // Create User
+        User::create([
+            "name" => $request->name,
+            "email" => $request->email,
+            "password" => Hash::make($request->password) 
+        ]);
+
+        return response()->json([
+            "status" => true,
+            "message" => "User created successfully"
+        ]);
+    }
+
+    //Login API (POST)
+
+    public function login(Request $request){
+
+    }
+
+    //Profile API (GET)
+
+    public function profile(){
+
+    }
+
+    //Logout
+
+    public function logout(){
+        
+    }
 }
---
+++ laravel/routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+// Syntax <URL>/api/register
+
+// Open Routes
+Route::post("register", [UserController::class, "register"]);
+Route::post("login", [UserController::class, "login"]);
+
+// Protected Routes
+Route::group([
+    "middleware" => ["auth:api"]
+], function(){
+
+    Route::get("profile", [UserController::class, "profile"]);
+    Route::get("logout", [UserController::class, "logout"]);
+});
